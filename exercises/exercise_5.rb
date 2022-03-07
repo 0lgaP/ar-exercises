882ccaf023c4a7f4d3c82b0d79840eb29a6055b4
0lgaP <perronolga@gmail.com>
2022-03-07 23:28:12 +0000

    sum, average, chaining methods

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,8 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+puts Stores.sum(:annual_revenue)
+puts "----------"
+puts Stores.average(:annual_revenue)
+puts "----------"
+puts Stores.where("annual_revenue > ?", 1000000).count
