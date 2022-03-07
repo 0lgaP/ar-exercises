88b97c19b5a60a9c6e84c70bd18719f421eac1be
0lgaP <perronolga@gmail.com>
2022-03-07 22:48:31 +0000

    ex 1 logic

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,6 +1,36 @@
 require_relative '../setup'
-
 puts "Exercise 1"
 puts "----------"
+
+class Stores < ActiveRecord::Base
+
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, length: {minimum: 0}
+            :apparel_type?
+  
+  def apparel_type?
+    if :mens_apparel || :womens_apparel
+      true
+    end
+  end
+
+end
+
+Stores.create(name: 'Burnaby', 
+              annual_revenue: 300000, 
+              mens_apparel: true,
+              womens_apparel: true)
+
+Stores.create(name: 'Richmond', 
+              annual_revenue: 1260000, 
+              mens_apparel: false,
+              womens_apparel: true)
+
+Stores.create(name: 'Gastown', 
+              annual_revenue: 190000, 
+              mens_apparel: true,
+              womens_apparel: false)
+
+puts Stores.count
 
 # Your code goes below here ...
