9d6be1bc319f501e78d51c14fd2b4f9d69779ce7
0lgaP <perronolga@gmail.com>
2022-03-07 22:56:17 +0000

    access and change name

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,10 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+@store1 = Stores.find(1)
+@store2 = Stores.find(2)
+
+@store1.name = "Spencer's Gifts"
+@store1.save
+
+puts Stores.find(1).name
